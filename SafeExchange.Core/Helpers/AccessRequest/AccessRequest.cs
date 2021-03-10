62aaa0a478f798309453b25928ed9e8b82b17321
yury-opolev <space.oyster@outlook.com>
2021-03-10 08:29:46 +0100

    Added request model

---
+++ SafeExchange.Core/Helpers/AccessRequest/AccessRequest.cs
@@ -0,0 +1,39 @@
+﻿/// <summary>
+/// SafeExchange 
+/// </summary>
+
+namespace SpaceOyster.SafeExchange.Core
+{
+    using System;
+
+    public class AccessRequest
+    {
+        public string id { get; set; }
+
+        public string PartitionKey { get; set; }
+
+        /// <summary>
+        /// User's name/id, who requests access
+        /// </summary>
+        public string SubjectName { get; set; }
+
+        /// <summary>
+        /// Secret name/id, which is the object to access
+        /// </summary>
+        public string ObjectName { get; set; }
+
+        /// <summary>
+        /// List of permissions, a comma-separated list
+        /// </summary>
+        public string Permissions { get; set; }
+
+        public DateTime RequestedAt { get; set; }
+
+        public RequestStatus Status { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id:{this.id}, Subject:{this.SubjectName}, Object:{this.ObjectName}, Permissions:'{this.Permissions}', RequestedAt:{this.RequestedAt}, Status:{this.Status}";
+        }
+    }
+}
---
+++ SafeExchange.Core/Helpers/AccessRequest/RequestStatus.cs
@@ -0,0 +1,17 @@
+﻿/// <summary>
+/// SafeExchange
+/// </summary>
+
+namespace SpaceOyster.SafeExchange.Core
+{
+    public enum RequestStatus
+    {
+        None = 0,
+
+        InProgress = 10,
+        
+        Approved = 20,
+        
+        Rejected = 30
+    }
+}
