8fe8c44731e3b9e0db6fcf870fa6d84055fa70f1
yury-opolev <space.oyster@outlook.com>
2023-10-30 21:13:30 +0100

    Minor changes

--- SafeExchange.Core/Functions/SafeExchangeProcessExternalNotification.cs
+++ SafeExchange.Core/Functions/SafeExchangeProcessExternalNotification.cs
@@ -6,6 +6,7 @@ namespace SafeExchange.Core.Functions
 {
     using Azure.Storage.Queues.Models;
     using Microsoft.Extensions.Logging;
+    using SafeExchange.Core.DelayedTasks;
     using System;
 
     public class SafeExchangeProcessExternalNotification
@@ -21,8 +22,26 @@ namespace SafeExchange.Core.Functions
         {
             log.LogInformation($"{nameof(SafeExchangeProcessExternalNotification)} triggered.");
 
+            var messageBody = message.Body?.ToString() ?? string.Empty;
+            WebhookNotificationTaskPayload? webhookNotificationTaskPayload;
+            try
+            {
+                webhookNotificationTaskPayload = DefaultJsonSerializer.Deserialize<WebhookNotificationTaskPayload>(messageBody);
+            }
+            catch
+            {
+                log.LogWarning($"Could not parse message body for notification processing, message will be discarded.");
+                return;
+            }
+
+            if (webhookNotificationTaskPayload == null || string.IsNullOrEmpty(webhookNotificationTaskPayload.WebhookNotificationDataId))
+            {
+                log.LogWarning($"Notification task payload is null or data id is empty, message will be discarded.");
+                return;
+            }
+
             // TODO
-            log.LogInformation($"Message body: {message.Body?.ToString()}.");
+            log.LogInformation($"Task payload: {webhookNotificationTaskPayload.TaskType}, {webhookNotificationTaskPayload.SubType}: {webhookNotificationTaskPayload.WebhookNotificationDataId}.");
             await Task.CompletedTask;
         }
     }
