6f9cd9c7ef65c99dec9361cc801e119b10d02427
yury-opolev <space.oyster@outlook.com>
2024-10-12 19:53:57 +0200

    Minor changes

---
+++ SafeExchange.Core/Migrations/Model/MigrationItem00004_1.cs
@@ -0,0 +1,26 @@
+﻿/// <summary>
+/// MigrationItem00004
+/// </summary>
+
+namespace SafeExchange.Core.Migrations
+{
+    using System;
+
+    public class MigrationItem00004_1
+    {
+        public MigrationItem00004_1()
+        { }
+
+        public MigrationItem00004_1(MigrationItem00004_1 source)
+        {
+            this.PartitionKey = source.PartitionKey;
+            this.id = source.id;
+        }
+
+        public string PartitionKey { get; set; }
+
+        public string id { get; set; }
+
+        public string GroupMail { get; set; }
+    }
+}
