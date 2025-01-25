f5efeb8aa265786cf487f81710b39c838f9571df
Yury Opolev <yuri.opolev@gmail.com>
2025-01-25 16:53:47 +0100

    Minor changes

---
+++ SafeExchange.Core/Migrations/Model/MigrationItem00007_Target.cs
@@ -0,0 +1,35 @@
+﻿/// <summary>
+/// MigrationItem00007
+/// </summary>
+
+namespace SafeExchange.Core.Migrations
+{
+    using SafeExchange.Core.Model;
+    using System;
+
+    public class MigrationItem00007_Target
+    {
+        public MigrationItem00007_Target()
+        { }
+
+        public MigrationItem00007_Target(string userId, MigrationItem00007 source)
+        {
+            this.PartitionKey = PinnedGroup.DefaultPartitionKey;
+            this.id = source.id;
+
+            this.UserId = userId;
+            this.GroupItemId = source.GroupId;
+            this.CreatedAt = DateTime.UtcNow;
+        }
+
+        public string PartitionKey { get; set; }
+
+        public string id { get; set; }
+
+        public string UserId { get; set; }
+
+        public string GroupItemId { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
